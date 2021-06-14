d1594f1d467da479a703833ba7a733a85be2c989
LeejuOh <xop4p@naverc.om>
2021-06-14 13:15:26 +0900

    add comment

--- greedy/book/q6.py
+++ greedy/book/q6.py
@@ -13,8 +13,11 @@ def solution(food_times, k):
     for i in range(len(food_times)):
         heapq.heappush(q, (food_times[i], i + 1))
 
+    # 먹기 위해 사용한 시간
     sum_value = 0
+    # 직전에 다 먹은 음식 시간
     previous = 0
+    # 남은 음식의 갯수
     length = len(food_times)
     while sum_value + ((q[0][0] - previous) * length) <= k:
         now = heapq.heappop(q)[0]
@@ -24,4 +27,3 @@ def solution(food_times, k):
 
     result = sorted(q, key=lambda x: x[1])
     return result[(k - sum_value) % length][1]
-    
