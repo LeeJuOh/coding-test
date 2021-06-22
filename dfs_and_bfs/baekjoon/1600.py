fc24c84cb0a8a73f2455aca508822f6b8f4ba42b
LeejuOh <xop4p@naverc.om>
2021-06-22 21:16:18 +0900

    fix 1600

--- dfs_and_bfs/baekjoon/1600.py
+++ dfs_and_bfs/baekjoon/1600.py
@@ -5,20 +5,25 @@ from collections import deque
 def solution(x, y, horse_count, distance):
     global W, H
 
+    def is_valid(x: int, y: int) -> bool:
+        global W, H
+
+        if x < 0 or y < 0 or x >= H or y >= W:
+            return False
+        return True
+
     def move(move_dx, move_dy, horse_count, distance, type=None):
         for dx, dy in zip(move_dx, move_dy):
             nx = x + dx
             ny = y + dy
-            if nx == H - 1 and ny == W - 1:
-                return True
-            if not is_valid(nx, ny) or board[nx][ny] == 1 or visited[distance][nx][ny]:
-                continue
 
             if type == 'horse':
                 horse_count += 1
-            queue.append((nx, ny, horse_count, distance + 1))
 
-        return False
+            if is_valid(nx, ny) and board[nx][ny] == 0 and \
+               not visited[nx][ny][horse_count]:
+                queue.append((nx, ny, horse_count, distance + 1))
+                visited[nx][ny][horse_count] = True
 
     board = []
     for _ in range(H):
@@ -30,30 +35,19 @@ def solution(x, y, horse_count, distance):
 
     queue = deque()
     queue.append((x, y, horse_count, distance))
-    visited = [[[False] * W for _ in range(H)] for _ in range(32)]
-    # print(len(visited))
-    # print(len(visited[0]))
-    # print(len(visited[0][0]))
-    flag1 = False
-    flag2 = False
+    visited = [[[False] * 31 for _ in range(W)] for _ in range(H)]
+    print(len(visited))
+    print(len(visited[0]))
+    print(len(visited[0][0]))
     while queue:
         x, y, horse_count, distance = queue.popleft()
-        visited[distance][x][y] = True
+        if x == H - 1 and y == W - 1:
+            return distance
 
         if horse_count < K:
-            flag1 = move(horse_dx, horse_dy, horse_count, distance, 'horse')
-        flag2 = move(monkey_dx, monkey_dy, horse_count, distance)
-        if flag1 or flag2:
-            return distance
+            move(horse_dx, horse_dy, horse_count, distance, 'horse')
+        move(monkey_dx, monkey_dy, horse_count, distance)
     return -1
-
-
-def is_valid(x: int, y: int) -> bool:
-    global W, H
-
-    if x < 0 or y < 0 or x >= H or y >= W:
-        return False
-    return True
 
 
 input = sys.stdin.readline
