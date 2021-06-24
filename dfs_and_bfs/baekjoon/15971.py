a3707b2155c1587ee3901571dceb94b6785b6d10
LeejuOh <xop4p@naverc.om>
2021-06-24 14:12:32 +0900

    fix 15971

--- dfs_and_bfs/baekjoon/15971.py
+++ dfs_and_bfs/baekjoon/15971.py
@@ -5,26 +5,27 @@ import sys
 # 또 python으로 하기가 까다로웠다. 재귀, 메모리, 재귀 초과 등등..
 
 
-def dfs(node: int, target: int, dist: int, max_value: int) -> None:
+def dfs(node: int, dist: int, max_value: int) -> None:
+    global result, end
     visited[node] = True
-    if node == target:
-        print(dist - max_value)
+    if node == end:
+        result = dist - max_value
         return
     for next_node, cost in graph[node]:
         if not visited[next_node]:
-            dfs(next_node, target, dist + cost, max(max_value, cost))
+            dfs(next_node, dist + cost, max(max_value, cost))
 
 
 sys.setrecursionlimit(10**6)
 input = sys.stdin.readline
 INF = int(1e9)
-NODE_COUNT, robot_a, robot_b = map(int, input().split())
+NODE_COUNT, start, end = map(int, input().split())
 visited = [False] * (NODE_COUNT + 1)
-dists = [INF] * (NODE_COUNT + 1)
-max_values = [-1] * (NODE_COUNT + 1)
 graph = [[] for _ in range(NODE_COUNT + 1)]
 for _ in range(NODE_COUNT - 1):
     start_node, end_node, cost = map(int, input().split())
     graph[start_node].append((end_node, cost))
     graph[end_node].append((start_node, cost))
-dfs(robot_a, robot_b, 0, 0)
+result = 0
+dfs(start, 0, 0)
+print(result)
